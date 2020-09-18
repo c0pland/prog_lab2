b5ebb636cbb42116045431f359e4d01f3a0c6332
Bogdan <>
2020-09-18 11:11:47 +0300

    che-to schitaet

---
+++ lab2.cpp
@@ -0,0 +1,105 @@
+﻿#include <iostream>
+
+#include <ctime>
+#include <chrono>
+
+
+
+struct MyData
+{
+	double a;
+	double b;
+	double e;
+	double (*function)(double x);
+	double sum;
+
+
+};
+
+MyData generateData(double a, double b, double e, double (*function)(double x), double sum)
+{
+	MyData myData;
+	myData.a = a;
+	myData.b = b;
+	myData.e = e;
+	myData.function = function;
+	myData.sum = sum;
+	return myData;
+}
+
+double calculate(void* args)
+{
+
+	MyData myData = *(MyData*)args;
+
+	double a = myData.a;
+	double b = myData.b;
+	double e = myData.e;
+	double h = e;
+	double x = a;
+	double sum = 0;
+	int finish = (b - a) / h;
+#pragma omp parallel for num_threads(3) reduction(+: sum) 
+	for(int i=0;i< finish;i++)
+	{
+		sum += h * myData.function(x);
+		if (x + h < b)
+		{
+			x += h;
+		}
+		else
+		{
+			x = b;
+
+		}
+	}
+	//((MyData*)args)->sum = myData.sum;
+	//std::cout << ((MyData*)args)->sum << std::endl;
+	return sum;
+
+}
+double cosinus(double x)
+{
+	return cos(x);
+}
+
+
+
+int main()
+{
+	auto time1 = std::chrono::high_resolution_clock::now();
+	const int N = 1;
+	double sum = 0;
+	double sums[N];
+	//pthread_t threads[N];
+	MyData data[N];
+	double a = 0, b = 6000;
+
+	for (int i = 0; i < N; i++)
+	{
+		data[i] = generateData((a + (i) * (b - a) / N), (a + (i + 1) * (b - a) / N), 0.001, cosinus, 0);
+		//pthread_create(&threads[i], NULL, calculate, &data[i]);
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		//pthread_join(threads[i], NULL);
+	}
+
+	
+	
+		double rez=calculate(&data[0]);
+	
+
+
+	
+	std::cout << rez << std::endl;
+	auto time2 = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(time2 - time1).count();
+	auto avrDur = duration;
+	std::cout << avrDur << std::endl;
+	//13938
+	//358k
+	return 0;
+};
+
