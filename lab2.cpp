5559ac85e083cf5a509e34e94e2489dbd2706dd7
Bogdan <41237949+c0pland@users.noreply.github.com>
2020-10-02 13:57:23 +0700

    Add files via upload
    lab 2 done

--- lab2.cpp
+++ lab2.cpp
@@ -29,7 +29,7 @@ MyData generateData(double a, double b, double e, double (*function)(double x), 
 
 double calculate(void* args)
 {
-
+	auto time1 = std::chrono::high_resolution_clock::now();
 	MyData myData = *(MyData*)args;
 
 	double a = myData.a;
@@ -39,11 +39,11 @@ double calculate(void* args)
 	double x = a;
 	double sum = 0;
 	int finish = (b - a) / h;
-#pragma omp parallel for num_threads(3) reduction(+: sum) 
+#pragma omp parallel for num_threads(6) reduction(+:sum) 
 	for(int i=0;i< finish;i++)
 	{
-		sum += h * myData.function(x);
-		if (x + h < b)
+		sum += h * myData.function(i*e);
+		/*if (i * e + h < b)
 		{
 			x += h;
 		}
@@ -51,10 +51,14 @@ double calculate(void* args)
 		{
 			x = b;
 
-		}
+		}*/
 	}
 	//((MyData*)args)->sum = myData.sum;
 	//std::cout << ((MyData*)args)->sum << std::endl;
+	auto time2 = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(time2 - time1).count();
+	auto avrDur = duration;
+	std::cout << "Time = " << avrDur << std::endl;
 	return sum;
 
 }
@@ -67,24 +71,22 @@ double cosinus(double x)
 
 int main()
 {
-	auto time1 = std::chrono::high_resolution_clock::now();
+	srand(time(NULL));
+	//auto time1 = std::chrono::high_resolution_clock::now();
 	const int N = 1;
 	double sum = 0;
 	double sums[N];
 	//pthread_t threads[N];
 	MyData data[N];
 	double a = 0, b = 6000;
-
-	for (int i = 0; i < N; i++)
+	data[0] = generateData(a, b, 0.001, cos, 0);
+	/*for (int i = 0; i < N; i++)
 	{
 		data[i] = generateData((a + (i) * (b - a) / N), (a + (i + 1) * (b - a) / N), 0.001, cosinus, 0);
-		//pthread_create(&threads[i], NULL, calculate, &data[i]);
-	}
+		
+	}*/
 
-	for (int i = 0; i < N; i++)
-	{
-		//pthread_join(threads[i], NULL);
-	}
+
 
 	
 	
@@ -93,13 +95,30 @@ int main()
 
 
 	
-	std::cout << rez << std::endl;
-	auto time2 = std::chrono::high_resolution_clock::now();
+	std::cout <<"Rez = "<< rez << std::endl;
+	/*auto time2 = std::chrono::high_resolution_clock::now();
 	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(time2 - time1).count();
 	auto avrDur = duration;
 	std::cout << avrDur << std::endl;
 	//13938
 	//358k
+	/*int N = 10000;
+	for (int j = 0; j < N; j++)
+	{
+		if (j % 100 == 0)std::cout << j << " " << std::endl;
+#pragma omp parallel for num_threads(8)  
+		for (int i = 0; i < 100000; i++)
+		{
+			
+			int x = rand() % 20;
+		}
+	}
+	auto time2 = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(time2 - time1).count();
+	auto avrDur = duration / N;
+	std::cout << avrDur << std::endl;
+	//6700 - 1 thread
+	//2200 - 4 thread*/
 	return 0;
 };
 
